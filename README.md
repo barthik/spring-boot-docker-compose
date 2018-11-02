4234901bec76f8149dca6a1483e9ed7575fd6225
Adam Bartoň <adam.barton.d@gmail.com>
2018-11-02 13:33:57 +0100

    Project init

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-spring-boot-docker-compose
+# Spring Boot 2 + Docker Compose
+Test controller available at `http://localhost:9090/` with basic info:
+
+```
+[uuuu-MM-dd'T'HH:mm:ss.SSS] container_id : port 
+[2018-11-02T11:27:40.370] af0dc15f20a5 : 8080
+```
+
+##Docker Compose
+Docker commands:
+```cmd
+docker-compose up
+docker-compose shutdown
+```
+
+- Shared drive error?
+  - Allow to share your drive in `Docker/Settings/Shared Drives`
+- MariaDB failed to start?
+  - Try to recreate `./mariadb` dir, run `docker system prune` to clean up all volumes and run again
+
+##Standalone
+Maven build command:
+```cmd
+mvn clean install dockerfile:build
+```
+
+Note if you want to push image to the docker add to your `~/.m2/settings.xml`:
+```xml
+<servers>
+  <server>
+    <id>docker.io</id>
+    <username>PLACE_HOLDER</username>
+    <password>PLACE_HOLDER</password>
+  </server>
+</servers>
+```
+And to the `.pom` file in `dockerfile-maven-plugin`:
+```xml
+<configuration>
+  <useMavenSettingsForAuth>true</useMavenSettingsForAuth>
+</configuration>
+
+```
+
+Docker commands:
+```cmd
+docker run -p 9090:8080 -t PLACE_HOLDER/docker-compose:1.0.3-SNAPSHOT
+```
---
+++ src/main/java/com/github/barthik/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.github.barthik.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+
+@RestController
+public class TestController {
+
+    private final Environment environment;
+
+    @Autowired
+    public TestController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping(value = "/")
+    public String test() throws UnknownHostException {
+        String hostname = InetAddress.getLocalHost().getHostName();
+        String port = environment.getProperty("local.server.port");
+        return new StringBuilder()
+                .append("[")
+                .append(LocalDateTime.now().toString())
+                .append("] ")
+                .append(hostname != null ? hostname : "[unknown]")
+                .append(" : ")
+                .append(port != null ? port : "[unknown]")
+                .toString();
+    }
+}
